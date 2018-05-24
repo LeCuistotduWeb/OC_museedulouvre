c8c095ba42af781664b2d4cce530212f737686c7
GaetanB <gaetan.boyron@gmail.com>
2018-05-24 20:55:42 +0200

    create service calculPrice

--- src/OC/LouvreBundle/Controller/DefaultController.php
+++ src/OC/LouvreBundle/Controller/DefaultController.php
@@ -13,4 +13,15 @@ class DefaultController extends Controller
     {
         return $this->render('@OCLouvre/Default/index.html.twig');
     }
+
+    public function testServiceAction()
+    {
+        //test du service permettant de choisir le tgarif du billet en fonction de la date anniversaire
+        $dateBirthday = '1991-06-01';
+        $price = $this->get('oc_louvre.calculprice')->calculeTicketPrices($dateBirthday);
+        
+        dump($price);
+
+        return $this->render('@OCLouvre/Default/index.html.twig');
+    }
 }
---
+++ src/OC/LouvreBundle/Service/CalculPrice.php
@@ -0,0 +1,47 @@
+<?php
+namespace OC\LouvreBundle\Service;
+
+
+class CalculPrice
+{
+    public function calculeTicketPrices($birthday){
+        $aujourdhui = date('Y-m-d');
+        //calcule le nombre d'années entre la date du jour et la date d'anniversaire
+        $interval = $this->calculAge($birthday);
+
+        if($interval < 4){  //tarif baby
+            return $price = 0;
+        }elseif ($interval > 4 && $interval < 12) { //tarif enfant
+            return $price = 8;
+        }elseif ($interval > 12 && $interval < 60) {    //tarif normal
+            return $price = 16;
+        }elseif ($interval > 60) {  //tarif senior
+            return $price = 12;
+        }
+        else {
+            return $price = 16;
+        }
+    }
+
+    public function reductionTicketPricesPourcent($price){
+        $reduction = $price * 0.25;
+        $price -=  $reduction;
+        return $price;
+    }
+
+    public function reductionTicketPrices(){
+        return $price = 10;
+    }
+
+    public function test(){
+        return 'ca fonctionne bien';
+    }
+
+    public function calculAge($dateBirthday)
+    {
+        $datetime1 = new \DateTime();
+        $datetime2 = new \DateTime($dateBirthday);
+        $age = $datetime1->diff($datetime2);
+        return  $age->format('%y');
+    }
+}

4c70dc2316ead752ad821a68042ab15283611b75
GaetanB <gaetan.boyron@gmail.com>
2018-05-24 20:23:30 +0200

    add view homepage

--- src/OC/LouvreBundle/Controller/DefaultController.php
+++ src/OC/LouvreBundle/Controller/DefaultController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /**
+     * page principale
+     */
     public function indexAction()
     {
         return $this->render('@OCLouvre/Default/index.html.twig');
---
+++ web/js/app.js
@@ -0,0 +1,23 @@
+$(function() {
+  $('.btn-horaires').on( "click", function() {
+    $('#horaires').removeClass('hide');
+  });
+  $('.btn-tarifs').on( "click", function() {
+    $('#tarifs').removeClass('hide');
+  });
+  $('.btn-contact').on( "click", function() {
+    $('#contact').removeClass('hide');
+  });
+
+  $('#horairesClose').on( "click", function() {
+    $('#horaires').addClass('hide');
+  });
+
+  $('#tarifsClose').on( "click", function() {
+    $('#tarifs').addClass('hide');
+  });
+
+  $('#contactClose').on( "click", function() {
+    $('#contact').addClass('hide');
+  });
+});
